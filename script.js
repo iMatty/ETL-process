6142a0d7c353edd8f3ffab142288e450be94a7ac
Mateusz Wielgus <mwmatty1@gmail.com>
2018-10-18 12:52:46 +0200

    fixed button locking

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 $(document).ready(function()
 {
+	$("#extract").prop("enabled",true);
 	$("#transform").prop("disabled",true);
 	$("#load").prop("disabled",true);
 	
 	$("#extract").click(function()
 	{
 		$(this).removeClass("btn btn-danger navbar-btn").addClass("btn btn-success navbar-btn");
-		$("#transform").prop("disabled",false);
+		$("#transform").prop("disabled",false);	
+		$("#extract").prop("disabled",true);
 	});
 	
 	$("#transform").click(function()
 	{
 		$(this).removeClass("btn btn-danger navbar-btn").addClass("btn btn-success navbar-btn");
 		$("#load").prop("disabled",false);
+		$("#transform").prop("disabled",true);
 	});
 	
 	$("#load").click(function()
 	{
 		$(this).removeClass("btn btn-danger navbar-btn").addClass("btn btn-success navbar-btn");
+		$("#load").prop("disabled",true);
 	});
 }
 );
