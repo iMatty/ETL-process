044e397fcc6ee69dd0389b99f22355bdcc842bc9
Mateusz Wielgus <mwmatty1@gmail.com>
2018-11-06 21:55:11 +0100

    transform and cleaning system implement (first 2 offers)

--- script.js
+++ script.js
@@ -33,9 +33,24 @@ $(document).ready(function()
 
 			
 		var str = $('#pobranyHtml').val();
-		var title = str.indexOf("<title>");
-		$('#transHtml').val($('#pobranyHtml').val().slice(title)); //wycina all do pozycji <title>
-	});
+
+		var titleStrony = str.indexOf("<title>") + 7;
+		var titleStronyKoniec = str.indexOf("</title>");
+		var s1 = $('#pobranyHtml').val().substring(titleStrony,titleStronyKoniec); //tytul strony
+
+		var title1 = str.indexOf('<a class=\\\"teaser'); // 1 ogloszenie
+		var titleKoniec1 = str.indexOf("</div>\\n</a>\\n\\n\\n");
+		var s2 = $('#pobranyHtml').val().substring(title1,titleKoniec1);
+
+		var title2 = str.indexOf('<a class=\\\"teaser', title1+1); // 2 ogloszenie
+		var titleKoniec2 = str.indexOf("</div>\\n</a>\\n\\n\\n", titleKoniec1+1);
+		var s3 = $('#pobranyHtml').val().substring(title2,titleKoniec2);
+
+		var doBoxa = s1 + " " + s2 + " " + s3;
+		doBoxa = doBoxa.replace(/\\n/g,""); // proby sprzatania xD
+		var box = $('#transHtml').val(doBoxa); // wklejanie do boxa
+});
+
 	
 	
 	
