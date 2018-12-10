7cdad0b83685144b73f1939a0171043b962d81c4
Mateusz Wielgus <mwmatty1@gmail.com>
2018-12-10 15:46:58 +0100

    added new filtering system

--- script.js
+++ script.js
@@ -28,10 +28,10 @@ $(document).ready(function()
 	{
 		$(this).removeClass("btn btn-danger navbar-btn").addClass("btn btn-success navbar-btn");
 		$("#load").prop("disabled",false);
-		$("#transform").prop("disabled",true);
-		
-		var str = $('#pobranyHtml').val();
+		$("#transform").prop("disabled",true);	
 
+		var str = $('#pobranyHtml').val();
+		
 		// iloœæ og³oszeñ
 		var a = '<div class=\\"content__counter\\">';					
 		var iloscOgloszenP = str.indexOf(a) + a.length;
@@ -39,62 +39,58 @@ $(document).ready(function()
 		var iloscOgloszen = $('#pobranyHtml').val().substring(iloscOgloszenP,iloscOgloszenK-12);
 		//$("#boxes").append("Pobrano " + iloscOgloszen + " ogloszen");
 
-		// ogloszenia petla
-		var title;									
-		var titleEnd;
-		var s;
-		var doBoxa="";
-		for (var i=0; i<15; i++) { 							
-			title = str.indexOf('<a class=\\\"teaser', title+i);
-			titleEnd = str.indexOf("</div>\\n</a>\\n\\n\\n");
-			s = $('#pobranyHtml').val().substring(title,titleEnd);
-			doBoxa += s;
+
+		// id_ogloszen
+		const id_ogloszen = [];
+		var p = 0;
+		var k = 0;
+		for (var i=0; i<15; i++) {				
+			p = str.indexOf('id=\\"offer-', p+i) + 11;
+			k = str.indexOf('\\"\\n', p);
+			var id_ogloszenia = $('#pobranyHtml').val().substring(p,k);		
+			id_ogloszen[i] = id_ogloszenia;
 		}
 
-		
+
+		// tytuly_ogloszen
+		const tytuly_ogloszen = [];
+		var p = 0;
+		var k = 0;
+		for (var i=0; i<15; i++) {				
+			p = str.indexOf('teaser__title', p+i) + 16;
+			k = str.indexOf('</h2>\\n', p);
+			var tytul_ogloszenia = $('#pobranyHtml').val().substring(p,k);		
+			tytuly_ogloszen[i] = tytul_ogloszenia;
+		}
+
+
+		// ceny_psiakow
+		const ceny_psiakow = [];
+		var p = 0;
+		var k = 0;
+		for (var i=0; i<15; i++) {				
+			p = str.indexOf('teaser__price\\', p+i) +36;
+			k = str.indexOf('\\n', p);
+			var cena_psiaka = $('#pobranyHtml').val().substring(p,k);		
+			ceny_psiakow[i] = cena_psiaka;
+		}
+		//$("#transHtml").append(ceny_psiakow[0]);
 
 
 
-/*
-		var title1 = str.indexOf('<a class=\\\"teaser'); // 1 ogloszenie
-		var titleKoniec1 = str.indexOf("</div>\\n</a>\\n\\n\\n");
-		var s1 = $('#pobranyHtml').val().substring(title1,titleKoniec1);
 
-		var title2 = str.indexOf('<a class=\\\"teaser', title1+1); // 2 ogloszenie
-		var titleKoniec2 = str.indexOf("</div>\\n</a>\\n\\n\\n", titleKoniec1+1);
-		var s2 = $('#pobranyHtml').val().substring(title2,titleKoniec2);
-*/
 
-		// proby sprzatania xD
-		//var doBoxa = s1 + " " + s2 + " " + s3;
-		doBoxa = doBoxa.replace(/\\n/g,""); 
-		doBoxa = doBoxa.replace(/"/g,"");
-		doBoxa = doBoxa.replace(/</g,"");
-		doBoxa = doBoxa.replace(/>/g,"");
-		doBoxa = doBoxa.replace(/a class=\\teaser \\   /g,"");
-		doBoxa = doBoxa.replace(/\\/g,"");
-		doBoxa = doBoxa.replace(/div class=teaser__foto/g,"");
-		doBoxa = doBoxa.replace(/div class=teaser__tags/g,"");
-		doBoxa = doBoxa.replace(/\/div/g,"");
-		doBoxa = doBoxa.replace(/.class=/g,"");
-		doBoxa = doBoxa.replace(/imgteaser__img lazyload/g,"");
-		doBoxa = doBoxa.replace(/\/      divteaser__loader        divteaser__content    divteaser__infoBox            h2/g,"");
-		doBoxa = doBoxa.replace(/li/g,"");
-		doBoxa = doBoxa.replace(/\/p/g,"");
-		doBoxa = doBoxa.replace(/\/ul/g,"");
-		doBoxa = doBoxa.replace(/pteaser__price/g,"");
-		doBoxa = doBoxa.replace(/divteaser__priceBox                        Zapytaj o cen./g,"");
-		doBoxa = doBoxa.replace(/\/h[0-9]/g,"");
-		doBoxa = doBoxa.replace(/.span/g,"");
-		doBoxa = doBoxa.replace(/ateaser teaser--bigPicture  /g,"");
-		doBoxa = doBoxa.replace(/ +(?= )/g,'');
 
-		doBoxa = doBoxa.replace(/id=dfp-srodek1.*\/script/g,"");
 
-		
+	   	// ogloszenia	
+		for (var i=0; i<15; i++) {
+			$("#transHtml").append("id_ogloszenia: " + id_ogloszen[i] + " tytul: " + tytuly_ogloszen[i] + " cena: " + ceny_psiakow[i] + " ");
+		}
+			
 
-	   			
-		var box = $('#transHtml').val(doBoxa); // wklejanie do boxa
+
+
+	
 });
 
 	
