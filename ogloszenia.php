8eca619e0fc335d93d92522d5245e1228b9745c9
Sudzio133 <32868975+Sudzio133@users.noreply.github.com>
2018-12-12 20:20:06 +0100

    Create ogloszenia.php

---
+++ ogloszenia.php
@@ -0,0 +1,28 @@
+
+<?php
+$servername = "sbazy.uek.krakow.pl";
+$username = "s194864";
+$password = "zJEvxGCm";
+$dbname = "s194864";
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM ogloszenia ORDER BY aktualizacja";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id og³oszenia: " . $row["id_ogloszenia"]. "; Tytu³: " . $row["tytul"] ."; Cena: " . $row["cena"] . "; Link do og³oszenia: " . $row["link"] . "; Ostatnia aktualizacja: " . $row["aktualizacja"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
